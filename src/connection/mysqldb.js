e8a58c2da15036594df3162c34719ee3496c3a42
Abdralghi <algifari843@gmail.com>
2021-05-07 10:41:05 +0700

    JC-1 update login

---
+++ src/connection/mysqldb.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "cargo",
+});
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
