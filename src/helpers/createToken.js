0bce4b10791e906d71116f659fc239de50e95d9e
dino9611 <aldinorahman36@gmail.com>
2021-05-07 11:21:38 +0700

    JC-6 tambah Helper

---
+++ src/helpers/createToken.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+//jc-6
+module.exports = {
+  createAccessToken: (data) => {
+    const key = "saitama";
+    const token = jwt.sign(data, key, { expiresIn: "2h" });
+    return token;
+  },
+  createEmailVerifiedToken: (data) => {
+    const key = "king";
+    const token = jwt.sign(data, key, { expiresIn: "1m" });
+    return token;
+  },
+  createTokenRefresh: (data) => {
+    const key = "puripuri";
+    const token = jwt.sign(data, key);
+    return token;
+  },
+  createTokenForget: (data) => {
+    const key = "mumen";
+    const token = jwt.sign(data, key, { expiresIn: "10m" });
+    return token;
+  },
+};
